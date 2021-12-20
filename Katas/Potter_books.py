c91be3fc6a2b55801c8403024940af96651b272e
atilaks <jaimefrancotorrecillas@gmail.com>
2021-12-20 16:58:15 +0100

    check

--- Katas/Potter_books.py
+++ Katas/Potter_books.py
@@ -1,12 +1,12 @@
 class Articles:
     def __init__(self):
-        self._select_books = []                     # Libros seleccionados
-        self._amount_books = 0                      # Cantidad de libros seleccionados
-        self._types_of_books = 7                    # Tipos posibles de libros
+        self._select_books = []  # Libros seleccionados
+        self._amount_books = 0  # Cantidad de libros seleccionados
+        self._types_of_books = 7  # Tipos posibles de libros
 
     def set_books(self, *args):
         """
-        Fija la seleccion entrante de libros
+        Fija la selección entrante de libros
         """
         for i in args:
             self._select_books.append(i)
@@ -25,13 +25,13 @@ class ShoppingBasket(Articles):
 
     def __init__(self):
         super().__init__()
-        self._shopping_basket = [0, 0, 0, 0, 0, 0, 0]         # Cesta de la compra
-        self._distinct_books = 0                        # Tipos de libros en cesta
-        self._unaccounted_books = self._amount_books    # Libros no contados
-        self._first_price = 8                           # Valor de cada libro
-        self._discount_list = [0, 5, 10, 20, 25, 40, 50]        # Lista de descuentos
-        self._price_without_discount = 0                # Precio sin descuento
-        self._price_with_discount = 0                   # Precio con descuento
+        self._shopping_basket = [0, 0, 0, 0, 0, 0, 0]  # Cesta de la compra
+        self._distinct_books = 0  # Tipos de libros en cesta
+        self._unaccounted_books = self._amount_books  # Libros no contados
+        self._first_price = 8  # Valor de cada libro
+        self._discount_list = [0, 5, 10, 20, 25, 40, 50]  # Lista de descuentos
+        self._price_without_discount = 0  # Precio sin descuento
+        self._price_with_discount = 0  # Precio con descuento
 
     def set_shopping_basket(self):
         """
@@ -42,13 +42,13 @@ class ShoppingBasket(Articles):
 
     def set_distinct_books(self):
         """
-        Fija cuantos libros distitos hay en la cesta
+        Fija cuantos libros distintos hay en la cesta
         """
         self._distinct_books = self._types_of_books - self._shopping_basket.count(0)
 
     def set_processed_books(self):
         """
-        Contaviliza los libros procesados
+        Contabiliza los libros procesados
         """
         self._unaccounted_books = sum(self._shopping_basket)
 
@@ -64,7 +64,8 @@ class ShoppingBasket(Articles):
     def get_price_with_discount(self):
         result = 0
         while self._unaccounted_books > 0:
-            result += self._distinct_books * self._first_price * (1 - self._discount_list[self._distinct_books - 1] / 100)
+            result += self._distinct_books * self._first_price * (
+                    1 - self._discount_list[self._distinct_books - 1] / 100)
             self._shopping_basket = [0 if x <= 0 else x - 1 for x in self._shopping_basket]
             self.set_distinct_books()
             self.set_processed_books()
@@ -79,3 +80,4 @@ class ShoppingBasket(Articles):
 
     def get_ticket(self):
         pass
+        # TODO: terminar la opción de ticket
