28771e97435166a7248d87989f086e99bb1f12e0
atilaks <jaimefrancotorrecillas@gmail.com>
2022-04-13 11:58:04 +0200

    13.4.22-mañana

--- Advance Digital Experts/My_stolen_bike_test.py
+++ Advance Digital Experts/My_stolen_bike_test.py
@@ -1,5 +1,8 @@
 import unittest
-from My_stolen_bike import Bike
+from My_stolen_bike import *
+
+bike_description = ["00001AAA", "rojo", "carretera", "desaparecida", "Jose García",
+                    "19/03/2022", "sin descripción", "sin dirección"]
 
 
 class TestStolenBike(unittest.TestCase):
@@ -14,18 +17,69 @@ class TestStolenBike(unittest.TestCase):
         # Assert
         self.assertEqual(expected, result)
 
+    def test_department_random_assignment(self):
+        # Arrange
+        department = Department()
+        expected = "randomized"
+
+        # Act
+        result = department.set_department()
+        if result in department._department:
+            result = "randomized"
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_unassigned_department(self):
+        # Arrange
+        bike = Bike()
+        # Department().available_department = False
+        # Department().set_department_availability()
+        bike.set_bike(bike_description)
+        expected = "not assigned"
+
+        # Act
+        result = bike.get_department()
+
+        # Assert
+        self.assertEqual(expected, result)
+
     def test_define_bike(self):
         # Arrange
         bike = Bike()
-        bike_description = ["00001AAA", "rojo", "carretera", "desaparecida", "Jose García",
-                            "19/03/2022", "sin descripción", "sin dirección"]
+        bike.set_bike(bike_description)
         expected = {"license": "00001AAA", "color": "rojo", "type": "carretera",
                     "status": "desaparecida", "owner": "Jose García", "date": "19/03/2022",
-                    "description": "sin descripción", "address": "sin dirección"}
-        bike.set_bike(bike_description)
+                    "description": "sin descripción", "address": "sin dirección",
+                    "department": "not assigned"}
 
         # Act
         result = bike.get_bike()
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_bike_status(self):
+        # Arrange
+        status = Bike()
+        expected = "desaparecida"
+        status.set_bike(bike_description)
+
+        # Act
+        result = status.set_status()
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_change_status(self):
+        # Arrange
+        status = Bike()
+        expected = "encontrada"
+        status.set_bike(bike_description)
+        status.get_status()
+
+        # Act
+        result = status.set_status()
 
         # Assert
         self.assertEqual(expected, result)
